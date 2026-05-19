d9f52a1913325fa11cb359280897936713ea6562
mirolampela <miro.lampela@gmail.com>
2026-05-19 18:47:45 +0300

    top tracks fetch added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.client import get_client
-from src.stats import get_top_artists
+from src.stats import get_top_artists, get_top_tracks
 
 client = get_client()
 user = client.current_user()
@@ -8,8 +8,15 @@ top_artists_short = get_top_artists(client, 5, "short_term")
 top_artists_med = get_top_artists(client, 5, "medium_term")
 top_artists_long = get_top_artists(client, 5, "long_term")
 
+top_tracks_short = get_top_tracks(client, 5, "short_term")
+top_tracks_med = get_top_tracks(client, 5, "medium_term")
+top_tracks_long = get_top_tracks(client, 5, "long_term")
+
 user_name =  user.get("display_name")
 print(user_name)
 print(top_artists_short)
 print(top_artists_med)
 print(top_artists_long)
+print(top_tracks_short)
+print(top_tracks_med)
+print(top_tracks_long)
--- src/stats.py
+++ src/stats.py
@@ -3,3 +3,9 @@ def get_top_artists(client, limit, time_range):
     items = response.get("items")
     top_artists = [item["name"] for item in items]
     return top_artists
+
+def get_top_tracks(client, limit, time_range):
+    response = client.current_user_top_tracks(limit=limit, time_range=time_range)
+    items = response.get("items")
+    top_tracks = [item["name"] for item in items]
+    return top_tracks
